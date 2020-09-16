75996d71a21bc12f5aa02cf61df508fd65455524
Wesley Reid <Wesley@Bayonet-Inc.local>
2020-09-16 07:26:10 -0400

    Add project files.

---
+++ BayonetTickets_Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace BayonetTickets_Android
+{
+    [Activity(Label = "Bayonet Tickets", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        WebView web_view;
+        public class WebClientViewer : WebViewClient
+        {
+            [Obsolete]
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+                view.LoadUrl(url);
+                return false;
+            }
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Window.RequestFeature(WindowFeatures.NoTitle);
+
+            web_view = FindViewById<WebView>(Resource.Id.web_View);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetWebViewClient(new WebClientViewer());
+            web_view.LoadUrl("http://18.219.73.179:80");
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
